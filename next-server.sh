d3798ad469589e20562d552d4c2dbb220f20ab6e
ascccx <151421661+ascccx@users.noreply.github.com>
2024-08-20 20:26:30 +0800

    Update next-server.sh

--- next-server.sh
+++ next-server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 当前 NeXT-Server 版本
-VERSION="0.3.9"
-
 # 获取 NeXT-Server 的最新版本
 LATEST_VERSION=$(wget -qO- "https://api.github.com/repos/The-NeXT-Project/NeXT-Server/releases/latest" | grep "tag_name" | cut -d '"' -f 4)
 DOWNLOAD_URL="https://github.com/The-NeXT-Project/NeXT-Server/releases/download/${LATEST_VERSION}/next-server-linux-amd64.zip"
@@ -15,10 +12,9 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 function show_menu() {
-    echo -e "${GREEN}NeXT-Server 一键脚本 [${VERSION}]${NC}"
+    echo -e "${GREEN}NeXT-Server 一键脚本${NC}"
     echo "一个基于Xray开发，支持多协议的专属后端框架"
-    echo "支持协议:"
-    echo "(1) Shadowsocks2022, (2) Trojan, (3) Vmess, (4) TUIC"
+    echo "支持协议: (1) Shadowsocks2022, (2) Trojan, (3) Vmess, (4) TUIC"
     echo "项目地址：https://github.com/The-NeXT-Project/NeXT-Server"
     echo ""
     echo "请选择要执行的操作："
